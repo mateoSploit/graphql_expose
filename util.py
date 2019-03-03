0fc57eb278e01f7ef8c281b6c9a6cf49c0889878
Matthew Moses <mmoses.web@gmail.com>
2019-03-03 00:52:32 -0600

    Added support for mutiline user input

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-# This file is part of graphql-expose
+# This file is part of graphql_expose
 # See https://github.com/mateoSploit/graphql_expose for more information
 # Copyright (C) Matthew Moses <mmoses.web@gmail.com>
 # This program is published under a MIT license
@@ -6,12 +6,6 @@
 """
 Helper functions
 """
-
-def ask(message):
-    """
-    Prompts a user for input
-    """
-    return input("[+] {message}".format(message=message))
 
 def log(message):
     """
@@ -24,3 +18,25 @@ def error(message):
     Prints an error message to the console
     """
     print("[-] {message}".format(message=message))
+
+def ask_multi(message):
+    """
+    Prompts a user and allows multi line entry
+    """
+    log("{message}. Use Ctrl-D when done.".format(message=message))
+    contents = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        contents.append(line)
+    
+    return "\n".join(contents)
+    
+
+def ask(message):
+    """
+    Prompts a user for input
+    """
+    return input("[+] {message} ".format(message=message))
