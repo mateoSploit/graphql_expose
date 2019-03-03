84a5774cf15c920c875ab64f5c1c1ce352ec797e
Matthew Moses <mmoses.web@gmail.com>
2019-03-03 00:50:21 -0600

    Added application custom exception

---
+++ exs.py
@@ -0,0 +1,11 @@
+# This file is part of graphql-expose
+# See https://github.com/mateoSploit/graphql_expose for more information
+# Copyright (C) Matthew Moses <mmoses.web@gmail.com>
+# This program is published under a MIT license
+
+"""
+Custom application exceptions
+"""
+
+class ApplicationCriticalError(Exception):
+    pass
