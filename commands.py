d3c035fac004a112d0370667ae6f2e694e723b67
Matthew Moses <mmoses.web@gmail.com>
2019-03-03 00:53:13 -0600

    Add command to query a graphql API

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-# This file is part of graphql-expose
+# This file is part of graphql_expose
 # See https://github.com/mateoSploit/graphql_expose for more information
 # Copyright (C) Matthew Moses <mmoses.web@gmail.com>
 # This program is published under a MIT license
@@ -7,7 +7,11 @@
 Support code from program commands
 """
 
+import requests
+from util import ask
+from util import ask_multi
 from util import log
+from util import error
 
 usage = """
     commands \t|\t description
@@ -20,3 +24,16 @@ def help():
     Prints usage information to the console
     """
     log(usage)
+
+def run_query(url):
+    return gql_call(url)
+
+def gql_call(url):
+    query = ask_multi("What's the query?")
+    log("Executing query: {query}".format(query=query))
+    request = requests.post(url, json={ "query": query })
+    if request.status_code == 200:
+        return request.json()
+    else:
+        error("Query failed. {error}".format(error=request.text))
+    
--- exs.py
+++ exs.py
@@ -1,4 +1,4 @@
-# This file is part of graphql-expose
+# This file is part of graphql_expose
 # See https://github.com/mateoSploit/graphql_expose for more information
 # Copyright (C) Matthew Moses <mmoses.web@gmail.com>
 # This program is published under a MIT license
