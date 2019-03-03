341cf9bd57536289cfef656301b246136cac0794
Matthew Moses <mmoses.web@gmail.com>
2019-03-02 22:14:51 -0600

    Base setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # graphql-expose
 A script to easily explore GraphQL APIs on the command line
+
+Written for Python3
---
+++ gqlexpose.py
@@ -0,0 +1,8 @@
+# This file is part of graphql-expose
+# See https://github.com/mateoSploit/graphql-expose for more information
+# Copyright (C) Matthew Moses <mmoses.web@gmail.com>
+# This program is published under a MIT license
+
+"""
+Main module
+"""
