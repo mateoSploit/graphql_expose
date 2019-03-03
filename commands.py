e1d26bbd03fdf99b2cb16c7df4e091dc3dcb19e7
Matthew Moses <mmoses.web@gmail.com>
2019-03-02 23:15:41 -0600

    Updated URL in commands.py header

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 # This file is part of graphql-expose
-# See https://github.com/mateoSploit/graphql-expose for more information
+# See https://github.com/mateoSploit/graphql_expose for more information
 # Copyright (C) Matthew Moses <mmoses.web@gmail.com>
 # This program is published under a MIT license
 
